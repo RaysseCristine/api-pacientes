ad393abfe4ca8d8982055b7b8033e5073a5aabf6
Raysse Teixeira <rayssecristine@hotmail.com>
2026-07-16 19:28:25 -0300

    Projeto inicial Spring Boot

---
+++ src/test/java/com/raysse/apipacientes/ApiPacientesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.raysse.apipacientes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiPacientesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
