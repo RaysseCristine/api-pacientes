9029c3a41d51bd1005b3a36dabbc87f3d58dc4bf
Raysse Teixeira <rayssecristine@hotmail.com>
2026-07-20 19:59:45 -0300

    Implementa endpoint GET para listar pacientes

--- src/main/java/com/raysse/apipacientes/controller/PacienteController.java
+++ src/main/java/com/raysse/apipacientes/controller/PacienteController.java
@@ -5,15 +5,23 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @RestController
 @RequestMapping("/pacientes")
 public class PacienteController {
 
-
         @GetMapping
-        public Paciente listarPaciente(){
+        public List<Paciente> listarPaciente(){
 
-                return new Paciente(1L, "Maria", 30);
+                List<Paciente> pacientes = new ArrayList<>();
+
+                pacientes.add(new Paciente(1L, "Maia", 30));
+                pacientes.add(new Paciente(2L, "João", 28));
+                pacientes.add(new Paciente(3L, "Liz", 25));
+
+                return pacientes;
         }
 }
 
